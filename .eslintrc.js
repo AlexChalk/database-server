49d0b70d7fc5ec36cf6fcb20faeb690ff419def3
Alex Chalk <alexchalk17@gmail.com>
2017-06-12 19:38:54 -0400

    Initialize eslint, karma, mocha, sinon, and chai
    Seed project with example tests

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "chai": false,
+    "sinon": false,
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "warn"
+  }
+};

ddba55469b0ff136fad168af8e4f3f04f2be0eae
Alex Chalk <alexchalk17@gmail.com>
2017-06-13 11:31:15 -0400

    Complete tech spec
    -Remove karma
    -Remove sinon
    -Add chai-http
    -Complete spec--all tests green

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+var pairs = {};
+
+app.get('/', function (req, res) {
+  res.send('Hello, world!');
+});
+
+app.get('/set', function (req, res) {
+  for(var key in req.query) {
+    pairs[key] = req.query[key];
+  }
+  res.redirect('/');
+});
+
+app.get('/get', function (req, res) {
+  for(var key in req.query) {
+    if (typeof pairs[key] === 'string') {
+      res.send(pairs[key]);
+    }
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+module.exports = { 
+  app: app,
+  pairs: pairs };
---
+++ test/routerSpec.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+var app = require('../app.js').app;
+var pairs = require('../app.js').pairs;
+chai.use(chaiHttp);
+
+describe('root page', function() {
+  it('loads successfully', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('stores params', function(done) {
+    chai.request(app)
+      .get('/set?john=jam')
+      .end(function (err, res) {
+        expect(pairs['john']).to.equal('jam');
+        done();
+      });
+  });
+  it('retrieves params', function(done) {
+    chai.request(app)
+      .get('/set?john=james')
+      .then(function () {
+        chai.request(app)
+          .get('/get?john')
+          .end(function (err, res) {
+            expect(res.text).to.equal('james');
+            done();
+          });
+      });
+  });
+});
